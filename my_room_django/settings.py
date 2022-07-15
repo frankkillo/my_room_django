8dd608c62d5fe8c48284e6e2079a623ba77da0de
Джабраил Курданов <dzabrailkurdanov@MacBook-Air-Dzabrail.local>
2022-07-15 23:49:44 +0300

    add static root

--- my_room_django/settings.py
+++ my_room_django/settings.py
@@ -128,13 +128,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "static"
 
 STATIC_URL = 'static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
