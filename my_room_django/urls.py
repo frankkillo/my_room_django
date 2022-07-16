0ab67f77f59a4525b356a936b458adf9f9321c38
Джабраил Курданов <dzabrailkurdanov@MacBook-Air-Dzabrail.local>
2022-07-16 18:50:05 +0300

    set conf to Render

--- my_room_django/urls.py
+++ my_room_django/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
@@ -23,4 +24,11 @@ urlpatterns = [
     path('api/v1/hotels/', include('hotel.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
