f9a636d51deeb774f9119220255a8e57c3367eee
Джабраил Курданов <dzabrailkurdanov@MacBook-Air-Dzabrail.local>
2022-07-17 13:15:37 +0300

    set static root

--- my_room_django/settings.py
+++ my_room_django/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
